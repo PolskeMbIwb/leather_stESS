390d894d8bc9b08342d26cd6aba48a69a774e062
sbaranov <stas.baranov.@mail.ru>
2022-09-03 13:36:26 +0300

    add get weather

--- bot_base/main.py
+++ bot_base/main.py
@@ -1,4 +1,5 @@
 import asyncio, os, random
+from business_logic import parse_weather
 from telegram import Bot, Update, Chat, StickerSet
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
@@ -7,7 +8,8 @@ TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 bot = Bot(token=TELEGRAM_TOKEN)
 
 
-async def ping(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def ping(update, context):
+    print(update['message']['text'])
     possible_phrases = ["Да-да, я жив", 'А?...Что?', "Чо по репману"]
     await context.bot.send_message(chat_id=update.effective_chat.id, 
                                    text=random.choice(possible_phrases))
@@ -19,11 +21,15 @@ async def gachi(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, 
                                    text='Куда ебать?')
 
+async def weather(update, context):
+    metro = update['message']['text'].split()[1]
+    await context.bot.send_message(chat_id=update.effective_chat.id,text=parse_weather(metro=metro.lower()))
 
-def init_handlers() -> tuple[CommandHandler]:
+def init_handlers():    
     handlers = (
         CommandHandler('ping', ping),
         CommandHandler('gachi', gachi),
+        CommandHandler('weather',weather),
     )
     return handlers
 
